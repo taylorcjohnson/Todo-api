35605e47f6799e2e20234c85d2244352199d023f
Taylor Johnson <taylor.christian.johnson@gmail.com>
2015-12-16 19:03:35 +0000

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 8080;
-var todos = [{
-    id: 1,
-    description: 'Go to the store',
-    completed: false,
-}, {
-    id: 2, 
-    description: 'Return library book',
-    completed: false,
-}, {
-    id: 3,
-    description: 'Walk the dog',
-    completed: true,
-}];
+var todos = [];
+var todoNextId = 1;
+
+// Middleware to parse http request body to json
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -39,6 +32,22 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send();
     }
     
+});
+
+// POST /todos
+// Use body-parser to access req body as body object
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    
+    // add id field
+    body.id = todoNextId++;
+    
+    // push body into array
+    todos.push(body);
+    
+    //console.log('description:' + body.description);
+    
+    res.json(body);
 });
 
 app.listen(PORT, function () {
