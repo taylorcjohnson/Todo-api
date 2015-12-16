99f77b14258d9f25edc95c3a187aeeeb8330b3f0
Taylor Johnson <taylor.christian.johnson@gmail.com>
2015-12-16 20:11:33 +0000

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 8080;
 var todos = [];
@@ -18,13 +20,10 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
     
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    // Underscore library using findWhere method to where the first object 
+    // in todos that has an id that matches what the user submitted
+    var matchedTodo = _.findWhere(todos, {id: todoId});
     
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -37,15 +36,20 @@ app.get('/todos/:id', function (req, res) {
 // POST /todos
 // Use body-parser to access req body as body object
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+    
+    // Underscore used to validate values
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	
+	body.description = body.description.trim();
     
     // add id field
     body.id = todoNextId++;
     
     // push body into array
     todos.push(body);
-    
-    //console.log('description:' + body.description);
     
     res.json(body);
 });
