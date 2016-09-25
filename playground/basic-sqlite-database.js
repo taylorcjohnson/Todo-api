de1ef78b695c39d3cb393bca730b8ee96dc1cfa7
Taylor Johnson <taylor.christian.johnson@gmail.com>
2016-09-25 18:44:05 +0000

    Add todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,43 +19,40 @@ var Todo = sequelize.define('todo', {
     }
 });
 
-sequelize.sync().then(function () {
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+    //force: true
+}).then(function () {
     console.log('Everything is synced');
     
-    // Find by id else 'todo not found'
-    Todo.findById(17).then(function (todo) {
-        if (todo) {
-           console.log(todo.toJSON()); 
-       } else {
-           console.log('Todo not found!');
-       }
-    });
-    
-    // Todo.create({
-    //     description: 'Walk the dog',
-    //     completed: false
-    // }).then(function (todo) {
-    //     return Todo.create({
-    //         description: 'Clean office'
-    //     });
+    // User.create({
+    //     email: 'bob@bob.com'
     // }).then(function () {
-    //     //return Todo.findById(1);
-    //     return Todo.findAll({
-    //         where: {
-    //             description: {
-    //               $like: '%office%'
-    //             } 
-    //         }
+    //     return Todo.create({
+    //         description: 'clean yard'
     //     });
-    // }).then(function (todos) {
-    //     if (todos) {
-    //         todos.forEach(function (todo) {
-    //             console.log(todo.toJSON());
-    //         });
-    //     } else {
-    //         console.log('no todo found!');
-    //     }
-    // }).catch(function (e) {
-    //     console.log(e);
+    // }).then(function (todo) {
+    //     User.findById(1).then(function (user) {
+    //         user.addTodo(todo);
+    //     });
     // });
+    
+    User.findById(1).then(function (user) {
+       user.getTodos({
+           where: {
+               completed: false
+           }
+       }).then(function (todos) {
+           todos.forEach(function (todo) {
+               console.log(todo.toJSON());
+           });
+       }); 
+    });
+
 });
